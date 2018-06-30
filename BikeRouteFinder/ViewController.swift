7c1da2bea2e8f219552289a6e9e8f16687a424c7
poszposz <jan.posz@netguru.pl>
2018-06-30 15:41:08 +0200

    Initial commit, empty project

---
+++ BikeRouteFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BikeRouteFinder
+//
+//  Created by Jan Posz on 30.06.2018.
+//  Copyright © 2018 agh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
